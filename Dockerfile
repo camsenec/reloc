79c4749ede1b09118a24ce2bb58b43d0576dd11b
Tomoya Tanaka <totana@ttu.ee>
2021-09-06 13:11:26 +0300

    Minor change

--- Dockerfile
+++ Dockerfile
@@ -13,4 +13,4 @@ COPY . /app/
 
 RUN pip3 install -r requirements.txt
 
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["python3", "manage.py", "runserver", "0.0.0.0:8080"]
--- edge_management/settings.py
+++ edge_management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'secret'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "172.17.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "172.17.0.1", "localhost"]
 
 host_ip = os.environ.get("HOST_IP")
 if host_ip != None:
